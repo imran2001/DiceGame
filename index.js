ca7b3e4ab1a26d46d13fa89ac9a8cbb6a1231270
IMRAN NAZIR <imrnnzr1@gmail.com>
2023-09-01 22:20:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomnumber2=Math.floor(Math.random()*6)+1;
+var randomimg1= "images/dice"+randomnumber1+".png";
+var randomimg2= "images/dice"+randomnumber2+".png";
+var img1=document.querySelectorAll("img")[0];
+img1.setAttribute("src",randomimg1);
+
+var img2=document.querySelectorAll("img")[1];
+img2.setAttribute("src",randomimg2);
+
+
+if(randomnumber1>randomnumber2)
+{
+    document.querySelector("h1").innerHTML="🚩Player 1 Wins! ";
+}
+else if (randomnumber2>randomnumber1)
+{
+    document.querySelector("h1").innerHTML="Player 2 Wins! 🚩";
+}
+else 
+{
+    document.querySelector("h1").innerHTML="Draw!";
+}
